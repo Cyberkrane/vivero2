2593eaf23fda652e2f9e43978264aa7e695153c0
cyberkrane <cyberkranegmail.com>
2022-12-07 15:16:24 -0300

    se resolvio ruteo y se creo contacto component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ImgComponent } from './pages/img/img.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/img', pathMatch:'full'},
+  {path:'img', component: ImgComponent},
+  {path:'contacto', component: ContactoComponent},
+  {path:'**', component: ImgComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
