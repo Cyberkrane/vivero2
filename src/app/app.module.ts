f068881da147a103e8f196ccd16a58b2491c9e8f
cyberkrane <cyberkranegmail.com>
2022-12-07 13:07:52 -0300

    se crearon todos las tabs necesarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './pages/img/img.component';
+import { NavTabsComponent } from './pages/nav-tabs/nav-tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent
+    ImgComponent,
+    NavTabsComponent
   ],
   imports: [
     BrowserModule,
